467342f8158b2048b3538bc4afdb827040b1c2e5
0BooM <149960425+0BooM@users.noreply.github.com>
2025-11-01 17:23:39 +0100

    Refactor login route to handle authentication errors and improve user feedback

--- routes/RegistrationRouter.js
+++ routes/RegistrationRouter.js
@@ -4,13 +4,18 @@ const RegistrationController = require("../controllers/RegistrationController");
 const passport = require("../passport");
 
 RegistrationRouter.get("/log-in", RegistrationController.getLoginForm);
-RegistrationRouter.post(
-  "/log-in",
-  passport.authenticate("local", {
-    successRedirect: "/",
-    failureRedirect: "/log-in",
-  })
-);
+RegistrationRouter.post("/log-in", (req, res, next) => {
+  passport.authenticate("local", (err, user, info) => {
+    if (err) return next(err);
+    if (!user) {
+      return res.render("registration/login", { error: info.message });
+    }
+    req.logIn(user, (err) => {
+      if (err) return next(err);
+      return res.redirect("/");
+    });
+  })(req, res, next);
+});
 
 RegistrationRouter.get("/sign-up", RegistrationController.getSignupForm);
 RegistrationRouter.post("/sign-up", RegistrationController.postSignupForm);
